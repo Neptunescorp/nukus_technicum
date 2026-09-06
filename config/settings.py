315cb92b2737b88cd953d4b5bc900ab9d546d5dc
Neptune <macstore@192.168.1.5>
2026-09-06 11:22:42 +0500

    Fix static files discovery

--- config/settings.py
+++ config/settings.py
@@ -127,7 +127,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+
 STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "/media/"
